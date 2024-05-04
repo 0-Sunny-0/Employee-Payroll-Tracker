ec19c552e94a2ca94d2263577dc356e0cc9c161a
Sunny Le Bunz <msafinch@gmail.com>
2024-05-04 01:40:50 -0400

    Edits made to folder structure, js file, and README has been updated to reflect finished project.

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # Employee-Payroll-Tracker
 
+
+## Links
+
+<a href="https://0-sunny-0.github.io/Employee-Payroll-Tracker/">Deployed Link</a><br>
+<a href="https://github.com/0-Sunny-0/Employee-Payroll-Tracker">GitHub Repo</a>
+
+
 ## Description
 
-Payroll tracker for company wide employees
+Payroll tracker for company wide employees. Upon visiting the page, user will be prompted to enter
+in their first name, last name, and salary information. The prompt will continue asking until user
+clicks cancel. The tracker will also provide the average salary data in the console log, as well as
+pick and congratulate a random winner from our list of employee names. 
 
 ## Visuals
 
-
+<img src="./assets/images/Payroll Tracker landing page.png"/>
+<img src="./assets/images/Payroll Tracker Data Entry.png"/>
+<img src="./assets/images/Payroll Tracker Console Log.png"/>
 
 ## License
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,64 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
+let employeesArray = []
+
 // Collect employee data
+// TODO: Get user input to create and return an array of employee objects
 const collectEmployees = function() {
-  // TODO: Get user input to create and return an array of employee objects
+
+ let continueAsking = true;
+
+  while(continueAsking){ 
+  const firstName = prompt('First Name');
+  const lastName = prompt('Last Name');
+  const salary = Number(prompt('Salary'));
+  console.log(firstName);
+  console.log(lastName);
+  console.log(salary);
+
+  let employees = {firstName, lastName, salary};
+  console.log(employees);
+
+  employeesArray.push(employees);
+  console.log(employeesArray);
+
+  continueAsking = confirm('Do you want to add another entry?');
 }
+  
+  return employeesArray;
+  // Code will never reach this line due to the return
+
+  // if (confirm('Do you want to add another entry?')) {
+  // } else {
+  //   continueAsking = false;
+  // }     
+} 
+
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
+  let sum = 0;
+
+  for (let i = 0; i < employeesArray.length; i++){
+  sum += employeesArray[i].salary;
+  // console.log(sum);
+  // console.log(employeesArray[i].salary);
+}  
+  let average = sum / employeesArray.length;
+  console.log(`The average employee salary between our employee(s) is ${average}`);
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  let randomElement = employeesArray[Math.floor(Math.random() * employeesArray.length)];
+  console.log(`Congratulations to ${randomElement.firstName} ${randomElement.lastName}, our random drawing winner!`);
+  console.log(employeesArray);
 }
+
+// Don't forget you're a rockstar :) 
 
 /*
   ====================
